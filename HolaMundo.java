35b971883ce7951cdd4e186f734b024666d9cb85
Patricio Mendez <patricio.mendez07@icloud.com>
2018-01-08 14:56:05 -0300

    se actualizo este archivo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,3 +1,9 @@
+/**
+* Muestra por pantalla la frase "¡Hola mundo!"
+*
+* @author genesis182
+*/
+
 public class HolaMundo {
   public static void main(String[] args) {
     System.out.println(*¡Hola mundo!*);
